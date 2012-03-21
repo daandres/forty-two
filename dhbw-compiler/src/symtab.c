c509da21ff6c2399d1449301eed4985d66579025
marcel <marcel@q.de>
2012-03-21 13:59:08 +0100

    Arbeit an SymTable

--- dhbw-compiler/src/symtab.c
+++ dhbw-compiler/src/symtab.c
@@ -7,16 +7,32 @@
 
 #include "symtab.h"
 
- sym_entry *sym_table = NULL;
+// sym_entry *sym_table = NULL;
 
-void * putsym(sym_entry *new_entry) {
-	printf("NEW Symbol in Symbol Table: %s", new_entry->name);
-	HASH_ADD_STR(sym_table, name, new_entry);
-	return 0;
-}
+//void * putsym(sym_entry *new_entry) {
+//	printf("NEW Symbol in Symbol Table: %s", new_entry->name);
+//	HASH_ADD_STR(sym_table, name, new_entry);
+//	return 0;
+//}
+//
+//sym_entry * getsym(char *search) {
+//	sym_entry *found_entry;
+//	HASH_FIND_STR(sym_table, search, found_entry);
+//	return found_entry;
+//}
 
-sym_entry * getsym(char *search) {
-	sym_entry *found_entry;
-	HASH_FIND_STR(sym_table, search, found_entry);
-	return found_entry;
-}
+ sym_union* searchGlobal(char varName[]) { /* Kann Funktion und Variable zurückliefern */
+	 printf("search %s Global startet.", varName);
+ }
+
+ sym_variable* searchLocal(char varName[], char funcName[]) { /* Kann nur Variable zurückliefern */
+	 printf("search %s Local in %s startet.", varName, funcName);
+ }
+
+ int insertSymGlobal(char varName[]) {
+	 printf("New Variable or Function %s in Local.", varName);
+ }
+
+ int insertSymLocal(char varName[], char funcName[]) {
+	 printf("New Variable %s in %s.", varName, funcName);
+ }
--- dhbw-compiler/src/symtab.h
+++ dhbw-compiler/src/symtab.h
@@ -11,15 +11,52 @@
 #include "uthash.h"
 #include <stdio.h>
 
-struct sym_entry {
-	char *name;
-	char *value;
+/* Überhaupt benötigt? */
+struct list_node {
+	struct sym_variable content;
+	struct var_list_node *next;
+};
+
+enum types {
+	voidType, intType, intArrayType
+};
+
+struct sym_variable {
+	char name[];
+	enum type varType;
+	int* address;
+	int size;
 	UT_hash_handle hh; /* makes this structure hashable */
 };
 
-typedef struct sym_entry sym_entry;
+struct sym_function {
+	char name[];
+	enum type returnType;
+	/* callVars => einfach verkettete Liste<sym_variable> oder doch Hashmap */
+	/* lokalVars => einfach verkettete Liste<sym_variable> oder doch Hashmap */
+	char interCode[] /* evtl. durch Pointer ersetzen */;
+};
 
-void *putsym();
-sym_entry *getsym();
+union variable_or_function {
+	struct sym_variable variable;
+	struct sym_function function;
+};
+
+struct sym_union {
+	int whichOne; /* 0 -> variable; 1 -> function */
+	union variable_or_funtion;
+	UT_hash_handle hh; /* makes this structure hashable */
+};
+
+typedef struct sym_variable sym_variable;
+
+typedef struct sym_function sym_function;
+
+typedef struct sym_union sym_union;
+
+sym_union* searchGlobal(); /* Kann Funktion und Variable zurückliefern */
+sym_variable* searchLocal(); /* Kann nur Variable zurückliefern */
+int insertSymGlobal();
+int insertSymLocal();
 
 #endif /* SYMTAB_H_ */
