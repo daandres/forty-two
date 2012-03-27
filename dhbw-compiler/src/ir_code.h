8def19d43790e72580c18b84b75ec078a08a133a
Daniel Andres <daniel@laptop>
2012-03-28 00:44:17 +0200

    ir_code changes

--- dhbw-compiler/src/ir_code.h
+++ dhbw-compiler/src/ir_code.h
@@ -9,32 +9,35 @@
 #define IR_CODE_H_
 
 #include <stdio.h>
+#define START 100
+
+/*
+ * This struct is for Expressions.
+ */
+typedef struct ETYPE {
+		int* true;
+		int* false;
+		int* next;
+		int quad;
+		char place[5];
+	} EXPTYPE;
 
 enum opcode {
-	ASSIGN,
-	ADD,
-	SUB,
-	MUL,
-	DIV,
-	MINUS,
-	//
-	IFEQ,
-	IFNE,
-	IFGT,
-	IFGE,
-	IFLT,
-	IFLE,
-	GOTO,
-	//
-	RETURN,
-	CALL
-	//
-	// MEMORY
-};
+	ASS, ADD, SUB, MULt, DIV, MIN,
 
+	IFEQ, IFNE, IFGT, IFGE, IFLT, IFLE, GOTO,
+
+	RET, CALL
+
+// MEMORY
+};
 
 void gen(enum opcode, int target, int op_left, int op_right, int paramcount);
 
-void writeIrCode();
+char* Newtemp();
+int* Makelist(int nquad);
+int* Merge(int* list1, int* list2);
+void Backpatch(int* list, int nquad);
+void Gen();
 
 #endif /* IR_CODE_H_ */
