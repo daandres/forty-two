655b00a07213f5011570859310fa00dbeb9b8f1f
Daniel Andres <daniel@laptop>
2012-04-09 19:43:36 +0200

    ir code... but i don't know what i did

--- dhbw-compiler/src/ir_code.h
+++ dhbw-compiler/src/ir_code.h
@@ -24,13 +24,13 @@ typedef struct IRTYPE {
 
 
 enum opcode {
-	ASS, ADD, SUB, MULt, DIV, MIN,
+	OP_ASSIGN, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_MIN,
 
-	IFEQ, IFNE, IFGT, IFGE, IFLT, IFLE, GOTO,
+	OP_IFEQ, OP_IFNE, OP_IFGT, OP_IFGE, OP_IFLT, OP_IFLE, OP_GOTO,
 
-	RET, CALL
+	OP_RETURN_VOID, OP_RETURN_VAL, OP_CALL_VOID, OP_PARAM,
 
-// MEMORY
+	OP_ARRAY_LOAD, OP_ARRAY_STORE
 };
 
 void gen(enum opcode, int target, int op_left, int op_right, int paramcount);
@@ -40,5 +40,6 @@ int* Makelist(int nquad);
 int* Merge(int* list1, int* list2);
 void Backpatch(int* list, int nquad);
 void Gen();
+
 
 #endif /* IR_CODE_H_ */
